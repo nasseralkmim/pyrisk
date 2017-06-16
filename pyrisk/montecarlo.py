f06ad1fcbfdf165f9cf8856775286249230a53b2
nasseralkmim <nasser.alkmim@gmail.com>
2017-06-16 15:35:56 -0300

    added generator of gumbel RV

--- pyrisk/montecarlo.py
+++ pyrisk/montecarlo.py
@@ -12,7 +12,7 @@ def normal(num_simulations, mu, sig):
         sig: standard deviation of distribution
 
     Returns:
-        rv: rv ~ N(mu, sig) as a numpy ndarray
+        rv: rv -> N(mu, sig) as a numpy ndarray
 
     """
     u = np.random.rand(num_simulations)  # uniform [0, 1]
@@ -24,11 +24,11 @@ def normal(num_simulations, mu, sig):
 def lognormal(num_simulations, mu, sig):
     """Generate lognormal random variable
 
-    In order to generate a RV X ~ lN(mu, sig), we use the following relation
+    In order to generate a RV X -> lN(mu, sig), we use the following relation
 
         z = (ln(x) - mu_ln) / sig_ln
 
-    where the RV ln(X) ~ N(mu_ln, sig_ln)
+    where the RV ln(X) -> N(mu_ln, sig_ln)
 
     Args:
         num_simulations: number of simulations
@@ -36,7 +36,7 @@ def lognormal(num_simulations, mu, sig):
         sig: standard deviation of RV distribution
 
     Returns:
-        rv: rv ~ lN(mu, sig) as a numpy ndarray
+        rv: rv -> lN(mu, sig) as a numpy ndarray
 
     """
     u = np.random.rand(num_simulations)  # uniform [0, 1]
@@ -46,4 +46,43 @@ def lognormal(num_simulations, mu, sig):
     mu_ln = np.log(mu) - 1/2 * sig_ln**2
 
     x = np.exp(mu_ln + z*sig_ln)
+    return x
+
+
+def gumbel_r(num_simulations, mu, sig):
+    """Generates Gumbel right skewed random variable
+
+    Gumbel right skewed, also knwon as extreme Type I,
+    distribution is given, using scipy notation, as:
+
+        f(x) = exp(-(x-loc)1/scale) exp(-exp(-(x-loc)1/scale))
+
+    where the scale and loc parameters are related to the mean and
+    standard deviation by,
+
+        1/scale = a = sqrt(pi**2/(6*sig**2))
+        loc = u = mu - 0.5772/a
+
+    Montecarlo procedure:
+       1. Generate u -> U[0, 1]
+       2. Generate x -> F[mu, sig] with
+
+          xi = Fx^{-1}(ui), Fx is CDF of F
+
+    Args:
+        num_simulations: number of simulations
+        mu: mean of the RV following lognormal distribution
+        sig: standard deviation of RV distribution
+
+    Returns:
+        x: rv -> Gumbel Right (mu, sig) as a numpy ndarray
+
+    """
+    u = np.random.rand(num_simulations)  # uniform [0, 1]
+
+    a = np.sqrt(np.pi**2/(6*sig**2))
+    loc = mu - 0.5772/a
+    scale = 1/a
+
+    x = stats.gumbel_r.ppf(u, loc=loc, scale=scale)
     return x
